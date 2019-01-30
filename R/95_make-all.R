d2dbcf14d2939f0305c295370f22d9b685c8e025
Elizabeth McBride <mcbrideelizabethc@gmail.com>
2019-01-29 17:48:41 -0800

    Update to MAKEFILE.

--- R/95_make-all.R
+++ R/95_make-all.R
@@ -1,7 +1,7 @@
 # This will be the eventual MAKEFILE-type thing with `source()` for each part of the code
 
 ## Make vector of the counties you want. 
-## I kind of want to have some big parameters like this out here instead of internal to scripts so I can run stuff from out here.
+
 
 library(here); library(fs)
 
@@ -13,7 +13,13 @@ if (!dir_exists(here("figs"))) {
   dir_create(here("figs"))
 }
 
-source(here("R/01_extract-all-chts-tables_slo-sb.R"))
+# This section will put together the file `places_slo_sb.rds` used throughout the analysis
+source(here("R/01_01_extract-all-chts-tables_slo-sb.R"))
+source(here("R/01_02_classify-places-HWSO.R"))
+source(here("R/01_03_time-in-min-since-3am.R"))
+source(here("R/01_04_activity-var-processing.R"))
+source(here("R/01_05_trip-dist-axx-processing.R"))
+source(here("R/01_06_pr-hh-pl-vars.R"))
 
 
 rmarkdown::render(here("README.Rmd"))
