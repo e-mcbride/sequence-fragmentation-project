b2cbc17a5e657b026272064993173b6244a5e7d9
Elizabeth McBride <mcbrideelizabethc@gmail.com>
2019-01-30 13:53:27 -0800

    Something weird happened with this file and it did not make it to the new branch?

---
+++ R/02_modify-place-file.R
@@ -0,0 +1,21 @@
+# 02_
+
+library(TraMineR); library(tidyverse); library(here)
+
+places_dat <- read_rds(here("data","places_slo_sb.rds"))
+
+### Recoding variables: Make personID variable, change starting times to 1 instead of 0 by adding one minute to everything, simpler place_types names, AND saving it as a data.frame so traminer can use it
+#' I did this because traminer requires the sequence data to have no values <1 
+#' 
+
+places_mod <- places_dat %>% 
+  mutate(pid = as.numeric(str_c(SAMPN, PERNO))) %>% 
+  #mutate(arrival_time_3 = as.integer(arrival_time_3)) #%>%
+  mutate(place_fac = as.factor(place_type), place_int = as.integer(place_fac)) %>%
+  mutate(arr_time3_add1 = arrival_time_3 + 1, dep_time3_add1 = departure_time_3 + 1) %>%
+  mutate(pltype = recode(place_type, Other = "O", Home = "H", Work = "W", School = "S")) %>%
+  data.frame()
+
+places_mod %>% readr::write_rds(here("data","modified_pldat.rds"))
+
+
