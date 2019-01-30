d6b0e899dc673ce34783b5eb886d5ac121104460
Elizabeth McBride <mcbrideelizabethc@gmail.com>
2019-01-29 17:53:50 -0800

    Adding to and updating MAKEFILE

--- R/95_make-all.R
+++ R/95_make-all.R
@@ -17,11 +17,14 @@ if (!dir_exists(here("figs"))) {
 source(here("R/01_01_extract-all-chts-tables_slo-sb.R"))
 source(here("R/01_02_classify-places-HWSO.R"))
 source(here("R/01_03_time-in-min-since-3am.R"))
-source(here("R/01_04_activity-var-processing.R"))
-source(here("R/01_05_trip-dist-axx-processing.R"))
+# source(here("R/01_04_activity-var-processing.R")) # BROKEN TIL I GET FILES FROM ADAM
+# source(here("R/01_05_trip-dist-axx-processing.R")) # BROKEN TIL I GET FILES FROM ADAM
 source(here("R/01_06_pr-hh-pl-vars.R"))
 
+# WARNING: ONLY RUN THE FOLLOWING AFTER YOU HAVE FIXED 04 AND 05
+# source(here("R/01_95_make-place-file.R")) 
 
+#####
 rmarkdown::render(here("README.Rmd"))
 ## I'm not really sure why this is being created, tbh
 file_delete("README.html")
