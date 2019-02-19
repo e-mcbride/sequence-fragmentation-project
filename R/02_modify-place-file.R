c360e8b253e8d49bf480baa05b8bd4f149cff96d
Elizabeth McBride <mcbrideelizabethc@gmail.com>
2019-02-19 12:23:56 -0800

    Cleanup

--- R/02_modify-place-file.R
+++ R/02_modify-place-file.R
@@ -9,9 +9,9 @@ places_dat <- read_rds(here("data","places_slo_sb.rds"))
 #' 
 
 places_mod <- places_dat %>% 
-  mutate(pid = as.numeric(str_c(SAMPN, PERNO))) %>% 
-  #mutate(arrival_time_3 = as.integer(arrival_time_3)) #%>%
-  mutate(place_fac = as.factor(place_type), place_int = as.integer(place_fac)) %>%
+  #mutate(pid = as.numeric(str_c(SAMPN, PERNO))) %>% 
+  mutate(pid = str_c(SAMPN, PERNO)) %>% 
+  #mutate(place_fac = as.factor(place_type), place_int = as.integer(place_fac)) %>%
   mutate(arr_time3_add1 = arrival_time_3 + 1, dep_time3_add1 = departure_time_3 + 1) %>%
   mutate(pltype = recode(place_type, Other = "O", Home = "H", Work = "W", School = "S")) %>%
   data.frame()
