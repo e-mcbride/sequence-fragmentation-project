cd2a3669479b7a2656778bfc3a9453da09f3d8b7
Elizabeth McBride <mcbrideelizabethc@gmail.com>
2019-02-19 15:49:12 -0800

    Cleaned up descriptive comments.

--- R/02_modify-place-file.R
+++ R/02_modify-place-file.R
@@ -6,15 +6,7 @@ places_dat <- read_rds(here("data","places_slo_sb.rds"))
 
 ### Recoding variables: Make personID variable, change starting times to 1 instead of 0 by adding one minute to everything, simpler place_types names, AND saving it as a data.frame so traminer can use it
 #' I did this because traminer requires the sequence data to have no values <1
-#' # Also Making the overall id variable, a shortened placetype variable, and recoding the already-included travel as "loop" and "transfer"
-
-# places_mod <- places_dat %>% 
-#   #mutate(pid = as.numeric(str_c(SAMPN, PERNO))) %>% 
-#   mutate(pid = str_c(SAMPN, PERNO)) %>% 
-#   #mutate(place_fac = as.factor(place_type), place_int = as.integer(place_fac)) %>%
-#   mutate(arr_time3_add1 = arrival_time_3 + 1, dep_time3_add1 = departure_time_3 + 1) %>%
-#   mutate(pltype = recode(place_type, Other = "O", Home = "H", Work = "W", School = "S")) %>%
-#   data.frame()
+#' # Also Making the overall id variable, a shortened placetype variable, and recoding the already-included travel as "loop" and "transfer" since those were wrongly placed in the "Other" category.
 
 
 places_mod <- places_dat %>% 
