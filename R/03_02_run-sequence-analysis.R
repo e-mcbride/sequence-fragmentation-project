a7ef2ff08540c000694be9488acbd651b74e5c4e
Elizabeth McBride <mcbrideelizabethc@gmail.com>
2019-02-27 14:16:36 -0800

    Updated time function to fix time column labels past midnight

--- R/03_02_run-sequence-analysis.R
+++ R/03_02_run-sequence-analysis.R
@@ -1,7 +1,6 @@
 library(tidyverse); library(TraMineR); library(here)
 
 travel_dat <- readr::read_rds(here("data", "places_travel-added.rds"))
-
 pl.labels <- seqstatl(travel_dat$place_type)
 
 
@@ -11,7 +10,11 @@ min2TOD = function(dayminutes) {
   for (i in 1:length(dayminutes)) {
     add3 = (dayminutes[i]/60) + 3
     
+    if(add3 < 24){
     tod_hr = (floor(add3)) %>% str_pad(width = 2, side = "left", pad = "0")
+    } else {
+      tod_hr = (floor(add3) - 24) %>% str_pad(width = 2, side = "left", pad = "0")
+    }
     
     tod_min = ((add3 - floor(add3))*60) %>% round() %>% 
       str_pad(width = 2, side = "left", pad = "0")
@@ -22,7 +25,10 @@ min2TOD = function(dayminutes) {
 }
 
 
-time <- min2TOD(seq(0,1399))
+
+
+time <- min2TOD(seq(0,1439))
+
 
 # Running sequence analysis:
 pl.seq <- travel_dat %>% data.frame() %>% 
