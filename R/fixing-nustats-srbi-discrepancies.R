fd1562781a06634a37d335ef4bc8107cdff72e21
Elizabeth McBride <mcbrideelizabethc@gmail.com>
2019-06-06 11:23:11 -0700

    Removing mode categorization work from the nustats/srbi harmonization script

--- R/fixing-nustats-srbi-discrepancies.R
+++ R/fixing-nustats-srbi-discrepancies.R
@@ -34,59 +34,6 @@ pl.new$MODE %>% unique
 place$MODE %>% unique
 
 
-# %>% 
-#   arrange(desc(n))
-
-#' car driving alone, car driving others, car passengers, transit/pool passengers, walk, other motorized, other nonmotorized
-
-# TOTTR is total people traveling on trip, tottr >1 is more than 1 person
-pl.mode <- place %>% 
-  left_join(modes, by = c("MODE")) %>%
-  mutate(mode.simple = 
-           case_when(
-             str_detect(new.mode, "PASSENGER") ~ "vehicle passenger",
-             str_detect(new.mode, "DRIVER") & PARTY < 2 ~ "vehicle driving alone",
-             str_detect(new.mode, "DRIVER") & PARTY > 1 ~ "vehicle driving others",
-             str_detect(new.mode, "WALK") ~ "walk",
-             str_detect(new.mode, "BIKE") ~ "bike",
-             str_detect(new.mode, "BUS") ~ "transit",
-             str_detect(new.mode, "METRO")  ~ "transit",
-             str_detect(new.mode, "STREET CAR") ~ "transit", 
-             str_detect(new.mode, "MOTORCYCLE") ~ "other motorized",
-             str_detect(new.mode, "STREET CAR") ~ "transit", 
-             str_detect(new.mode, "SHUTTLE") ~ "transit", 
-             str_detect(new.mode, "") ~ "other non-motorized", 
-             TRUE ~ "other"
-           )) %>% 
-  select(new.mode, mode.simple) %>% 
-  distinct()
-
-
-# modes %>% 
-#   # left_join(modes, by = c("MODE")) %>%
-#   mutate(mode.simple = 
-#            case_when(
-#              str_detect(new.mode, "PASSENGER") ~ "car passenger",
-#              str_detect(new.mode, "DRIVER") ~ "driver",
-#              str_detect(new.mode, "WALK") ~ "walk",
-#              str_detect(new.mode, "BIKE") ~ "bike",
-#              str_detect(new.mode, "BUS") ~ "PT",
-#              TRUE ~ "other"
-#            )) %>% 
-#   View
-
-
-place %>% 
-  select(SAMPN, PERNO, PLANO, PARTY, HHMEM) %>% 
-  # distinct() %>% 
-  mutate(party.n = as.numeric(PARTY),
-         hhmem.n = as.numeric(HHMEM),
-         party.n - hhmem.n) %>% 
-  View()
-
-place %>% group_by(PARTY) %>% count() %>% View()
-place %>% group_by(HHMEM) %>% count() %>% View()
-
 #####
 # Fixing the messed up APURP variable with a crosswalk
 #####
