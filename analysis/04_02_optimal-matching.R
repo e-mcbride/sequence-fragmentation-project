13ce5aa3705b86b0989af04f9ab0f4562113328b
Elizabeth McBride <mcbrideelizabethc@gmail.com>
2019-06-12 18:32:18 -0700

    adding the substitution cost matrix

--- analysis/04_02_optimal-matching.R
+++ analysis/04_02_optimal-matching.R
@@ -4,11 +4,19 @@ library(TraMineR)
 
 pl.seq <- read_rds(here("results","place_sequence.rds"))
 
-counts <- TraMineR::seqsubm(pl.seq, method = "TRATE")
-round(counts, 2)
+couts <- TraMineR::seqsubm(pl.seq, method = "TRATE", with.missing = TRUE) # took about 60 seconds
+round(couts, 2)
+#      H->  O-> S->  T->  W-> *->
+# H-> 0.00 2.00   2 1.98 2.00   2
+# O-> 2.00 0.00   2 1.96 2.00   2
+# S-> 2.00 2.00   0 2.00 2.00   2
+# T-> 1.98 1.96   2 0.00 1.99   2
+# W-> 2.00 2.00   2 1.99 0.00   2
+# *-> 2.00 2.00   2 2.00 2.00   0
 
-pl.om <- TraMineR::seqdist(pl.seq, method = "OM", indel = 3, sm = couts)
+gc()
 
+pl.om <- TraMineR::seqdist(pl.seq, method = "OM", indel = 3, sm = couts, with.missing = TRUE)
 
 write_rds(pl.om, here("data", "sequences-optimal-matching.rds"))
 
