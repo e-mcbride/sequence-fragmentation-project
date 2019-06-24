9ec19c7e5cbd64044e2aa513f06da68c2f77ec62
Elizabeth McBride <mcbrideelizabethc@gmail.com>
2019-06-24 12:55:12 -0700

    Cleanup of seq analysis script

--- R/03_02_run-sequence-analysis.R
+++ R/03_02_run-sequence-analysis.R
@@ -28,36 +28,22 @@ min2TOD = function(dayminutes) {
 time <- min2TOD(seq(0,1439))
 
 
-# ironing out some issues
-travel_dat %>% mutate(timedif = dep_time3_add1 - arr_time3_add1) %>% View()
+# bad.ids <- travel_dat %>%  mutate(timedif = dep_time3_add1 - arr_time3_add1) %>% 
+#   filter(timedif<0) %>% 
+#   .$pid
+# 
+# temp.traveldat <- travel_dat %>% filter(!(pid %in% bad.ids))# filter(!is.na(arr_time3_add1))
 
-# travel_dat %>% filter(pid=="70833711") %>% 
-#   arrange(pid, dep_time3_add1) %>% View()
-
-
-bad.ids <- travel_dat %>%  mutate(timedif = dep_time3_add1 - arr_time3_add1) %>% 
-  filter(timedif<0) %>% 
-  .$pid
-
-temp.traveldat <- travel_dat %>% filter(!(pid %in% bad.ids))# filter(!is.na(arr_time3_add1))
-
-# also need to investigate the NA's:
-
-# check for start/end time consistency
-
-# investigate to make sure all start at time 1, end at time 1439
-
-
-
-
-# FORNOW: I will run an analysis excluding these people as an experiment to see how it goes. TOMORROW I will investigate who they are further
 
 # Running sequence analysis:
-pl.seq <- temp.traveldat %>% data.frame() %>% 
+pl.seq <- travel_dat %>% data.frame() %>% 
   seqdef(var = c("pid", "arr_time3_add1", "dep_time3_add1", "pltype"),
          informat = "SPELL", labels = pl.labels, process = FALSE, cnames = time, xtstep= 180)
 
 print(pl.seq[10000:10015, ], format = "SPS")
+
+print(pl.seq[1:15, ], format = "SPS")
+
 
 write_rds(pl.seq, here("results","place_sequence.rds"))
 
