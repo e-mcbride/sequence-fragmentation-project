3d9d36c0dd9b979ef8a85762d729fab7fc499e74
Elizabeth McBride <mcbrideelizabethc@gmail.com>
2019-06-24 13:37:56 -0700

    Adding code to get the subset of the sequences and write that to file too

--- analysis/05_01_sampling-households.R
+++ analysis/05_01_sampling-households.R
@@ -5,21 +5,24 @@ library(tidyverse); library(here)
 #' 1. get the final result that goes into the sequence analysis (b/c it contains the final sample used)
 pl_seq <- read_rds(here::here("results","place_sequence.rds")) %>% 
   as_tibble(rownames = "pid") %>% 
-  mutate(pid = as.numeric(pid))
+  mutate(pid = as.numeric(pid)) %>% 
+  mutate(SAMPN = str_sub(pid, end = -2))
 
 #' 2. Pull out the unique household ID's from it
 hhids <- pl_seq %>% 
-  transmute(SAMPN = str_sub(pid, end = -2)) %>% 
+  select(SAMPN) %>% 
   distinct 
 
 
 #' 3. Pick a random sample from it
-hhid_samp <- hhids %>% 
+hhid_samp15 <- hhids %>% 
   sample_n(size = 15000, replace = FALSE) %>% 
   .$SAMPN
 
 #' 4. write the sample list
-hhid_samp %>% readr::write_rds(here("data","sample-hhids-15000.rds"))
+hhid_samp15 %>% readr::write_rds(here("data","sample-hhids-15000.rds"))
+
+
 
 
 ##### 
@@ -27,12 +30,30 @@ hhid_samp %>% readr::write_rds(here("data","sample-hhids-15000.rds"))
 #####
 
 #' 3. Pick a random sample from it
-hhid_samp <- hhids %>% 
+hhid_samp05 <- hhids %>% 
   sample_n(size = 5000, replace = FALSE) %>% 
   .$SAMPN
 
 #' 4. write the sample list
-hhid_samp %>% readr::write_rds(here("data","sample-hhids-05000.rds"))
+hhid_samp05 %>% readr::write_rds(here("data","sample-hhids-05000.rds"))
+
+
+# pl_seq <- read_rds(here("results","place_sequence.rds"))
+
+seq_samp15 <- pl_seq %>% 
+  filter(SAMPN %in% hhid_samp15) %>% 
+  select(-SAMPN) %>% 
+  column_to_rownames(var = "pid")
+
+seq_samp15 %>% readr::write_rds(here("data","seq-samp-15000.rds"))
+
+
+seq_samp05 <- pl_seq %>% 
+  filter(SAMPN %in% hhid_samp05) %>% 
+  select(-SAMPN) %>% 
+  column_to_rownames(var = "pid")
+
+seq_samp05 %>% readr::write_rds(here("data","seq-samp-05000.rds"))
 
 
 
