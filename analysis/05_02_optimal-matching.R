0c4a4051eb4668f22b6295208c96d122585c1b10
Elizabeth McBride <mcbrideelizabethc@gmail.com>
2019-07-10 14:00:34 -0700

    Minor changes to OM script

--- analysis/05_02_optimal-matching.R
+++ analysis/05_02_optimal-matching.R
@@ -2,22 +2,21 @@
 library(tidyverse);library(here)
 library(TraMineR)
 
-seq_samp05 <- readr::read_rds(here("data","seq-samp-05000.rds"))
+seq_samp05 <- readr::read_rds(here::here("data","sample-seq-05000.rds"))
 
 
-couts <- TraMineR::seqsubm(seq_samp05, method = "TRATE", with.missing = TRUE) # took about 60 seconds
+couts <- TraMineR::seqsubm(seq_samp05, method = "TRATE")#, with.missing = TRUE) # took about 60 seconds
 round(couts, 2)
-#      H->  O-> S->  T->  W-> *->
-# H-> 0.00 2.00   2 1.98 2.00   2
-# O-> 2.00 0.00   2 1.96 2.00   2
-# S-> 2.00 2.00   0 2.00 2.00   2
-# T-> 1.98 1.96   2 0.00 1.99   2
-# W-> 2.00 2.00   2 1.99 0.00   2
-# *-> 2.00 2.00   2 2.00 2.00   0
+#      H->  O-> S->  T-> W->
+# H-> 0.00 2.00   2 1.99   2
+# O-> 2.00 0.00   2 1.98   2
+# S-> 2.00 2.00   0 2.00   2
+# T-> 1.99 1.98   2 0.00   2
+# W-> 2.00 2.00   2 2.00   0
 
-gc()
 
-pl_om <- TraMineR::seqdist(seq_samp05, method = "OM", indel = 3, sm = couts, with.missing = TRUE)
+pl_om <- TraMineR::seqdist(seq_samp05, method = "OM", indel = 3, sm = couts)#, with.missing = TRUE)
+# with 5000 households (12704 people), it took 1.135 days (about 27 hrs)
 
 write_rds(pl_om, here("data", "sequences-optimal-matching.rds"))
 
