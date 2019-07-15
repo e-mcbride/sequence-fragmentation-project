51ccd2e825102c1d921347da9d21bd720f77017c
Elizabeth McBride <mcbrideelizabethc@gmail.com>
2019-07-15 15:56:59 -0700

    Updating daily sequence by cluster plot script to use the cluster names

--- analysis/07_02_fig-daily-seq-by-cluster.R
+++ analysis/07_02_fig-daily-seq-by-cluster.R
@@ -1,5 +1,5 @@
-library(tidyverse); library(TraMineR);library(here)
-
+library(tidyverse); library(here)
+library(TraMineR)
 # Figure: Daily Sequences by Cluster 
 
 #####
@@ -16,32 +16,39 @@ colnames(pl_seq) <- seq(1,1440) #paste0("y", seq(1,1440))
 
 ##' 2. Extracting cluster ids by pid from `alldata`
 cluster_id <- alldata %>%
-  select(pid, cluster) %>%
-  group_by(cluster) %>% 
+  select(pid, namedcluster) %>%
+  group_by(namedcluster) %>% 
   mutate(n_clust = n()) %>%
   ungroup() %>%
-  mutate(cluster_count = paste0(cluster, " (n=", n_clust,")"))  %>%
+  mutate(cluster_count = paste0(namedcluster, " (n=", n_clust,")"))  %>%
   select(-n_clust)
 
 ##' 3. Join the cluster ids to the sequences by pid. Make data "long" so ggplot likes it
 seq_clust_long <- pl_seq %>% as_tibble(rownames = "pid") %>% 
   mutate(pid = as.numeric(pid)) %>%
   left_join(cluster_id, by = "pid") %>%
-  gather(key = "minute", value = "state",  -pid,-cluster, -cluster_count) %>%
+  gather(key = "minute", value = "state",  -pid,-namedcluster, -cluster_count) %>%
   mutate(minute =  as.numeric(minute)) %>%
   arrange(pid, minute)
 
 ##' 4. Make x axis labels (times)
-timelabs <- c(seq(3,21,3) %>% str_pad(width = 2,side = "left", pad = "0") %>% paste0(":00"), "00:00", "02:59")
+# timelabs <- c(seq(3,21,3) %>% str_pad(width = 2,side = "left", pad = "0") %>% paste0(":00"), "00:00", "02:59") # 9 labels
+
+timelabs <- c(seq(3,23,4) %>% str_pad(width = 2,side = "left", pad = "0") %>% paste0(":00"), "02:59") # 7 labels
+# timelabs
+
+# timelabs5 <- c(seq(3,24,6) %>% str_pad(width = 2,side = "left", pad = "0") %>% paste0(":00"), "02:59")
+# timelabs5
 
 #' MAKE FIGURE w/ `ggplot2`
 seq_clust_gg <- seq_clust_long %>% 
-  # filter(cluster == "School Day") %>%
+  # filter(namedcluster == "School Day" | namedcluster =="Traveling") %>%
   ggplot(aes(x = minute, fill =state)) 
 
 seq_clust_gg + geom_area(stat="bin", binwidth = 1, position = "fill") +
   scale_x_continuous('Time of Day', 
-                     breaks=seq(1,1620, by = 180), 
+                     # breaks=seq(1,1620, by = 180), # 9 labels
+                     breaks=seq(1,1620, by = 240), # 7 labels
                      minor_breaks = seq(1,1441, by = 60),
                      labels = timelabs) +
   scale_y_continuous(name = "Frequency")+
@@ -53,16 +60,15 @@ seq_clust_gg + geom_area(stat="bin", binwidth = 1, position = "fill") +
   theme_bw() + 
   theme(
     text = element_text(size=8),
-    axis.text = element_text(size=7),
+    axis.text = element_text(size=6.5),
     legend.text = element_text(size=7),
-    #strip.background = , 
     strip.text = element_text(size=8),
     panel.spacing.x = unit(0.28, "cm"),
-    panel.border = element_rect(size = 0.4),
+    panel.border = element_rect(size = 0.2),
     axis.ticks = element_line(size = 0.4)
   )
 
-ggsave(here::here("figs","09-cluster-daily-sequences.png"), units = "in", width = 6.4, height = 5.2)
+ggsave(here::here("figs","09-cluster-daily-sequences.png"), units = "in", width = 6.5)#, height = 5.2)
 
 
 
