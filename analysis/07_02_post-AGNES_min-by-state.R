ce0747bf1de591a32dbdfb68a736aa05c204bade
Elizabeth McBride <mcbrideelizabethc@gmail.com>
2019-07-15 17:28:29 -0700

    First commit: script for minutes by state

---
+++ analysis/07_02_post-AGNES_min-by-state.R
@@ -0,0 +1,33 @@
+# Minutes spent in each state: prepping for travel time ratio
+
+library(tidyverse)
+
+seq_samp <- readr::read_rds(here::here("data", "sample-seq-05000.rds"))
+
+min_by_state <- seq_samp %>% 
+  as_tibble(rownames = "pid") %>% 
+  gather(-pid, key = "time", value = "place") %>% 
+  group_by(pid, place) %>% 
+  count(name = "minutes") %>% 
+  ungroup() %>% 
+  spread(key= place, value = "minutes", fill = 0) %>% 
+  rename(min_H = "H", min_O = "O", min_S = "S", min_T = "T", min_W = "W") %>% 
+  mutate(min_at_nonhome_places = min_O + min_S + min_W)
+
+
+min_by_state %>% write_rds(here("data", "min-by-state.rds"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
